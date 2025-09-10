c6cdb7afae52221d5f2f417f43dbf1540e0c45db
gustavogogola91 <gustavogogola3@gmail.com>
2025-09-10 19:53:27 -0300

    Adicionado base da seção Doe

---
+++ src/components/Doe.tsx
@@ -0,0 +1,13 @@
+export default function Doe() {
+
+    return(
+        <section className="w-[80%] md:w-[90%] flex flex-col gap-4 m-auto text-center mt-4">
+            <h2 className="text-2xl">Doe</h2>
+            <h3>Nos auxilie em nossa missão</h3>
+            <div className="flex flex-col gap-8 mt-8">
+                <img src="/QrCodeEemplo.png" alt="Qrcode" className="w-50 m-auto"/>
+                <p>Chave pix: 00.000.000/0000-00</p>
+            </div>
+        </section>
+    )
+}
