43f5a05bda6af85588425c1067a14b55419f44be
gustavogogola91 <gustavogogola3@gmail.com>
2025-09-25 13:26:37 -0300

    Alteração de estilização do título

--- src/components/Contato.tsx
+++ src/components/Contato.tsx
@@ -1,7 +1,7 @@
 export default function Contato() {
   return (
     <section className="w-[80%] md:w-[90%] flex flex-col gap-4 m-auto text-center mt-4">
-      <h2 className="text-2xl">Nossas redes sociais</h2>
+      <h2 className="text-2xl text-[#4A755A] uppercase">Nossas redes sociais</h2>
       <h3>Entre em contato conosco</h3>
       <ul className="flex flex-col lg:flex-row w-full gap-8 justify-center items-center">
         <li className="w-fit">
