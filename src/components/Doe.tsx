c86875c4547829a09e0e54bcc5962b22c285b033
gustavogogola91 <gustavogogola3@gmail.com>
2025-09-25 12:58:18 -0300

    Alterado nome do arquivo

--- src/components/Doe.tsx
+++ src/components/Doe.tsx
@@ -5,8 +5,11 @@ export default function Doe() {
             <h2 className="text-2xl">Doe</h2>
             <h3>Nos auxilie em nossa missão</h3>
             <div className="flex flex-col gap-8 mt-8">
-                <img src="/QrCodeEemplo.png" alt="Qrcode" className="w-50 m-auto"/>
+                <img src="/qrcode.png" alt="Qrcode" className="w-50 m-auto"/>
                 <p>Chave pix: 00.000.000/0000-00</p>
+            </div>
+            <div>
+                <p>Também é possível nos auxiliar doando suas notas fiscais por meio do programa Nota Paraná</p>
             </div>
         </section>
     )
