ae03edca17822996d7a4c3b8de5d783e12982bb3
gustavogogola91 <gustavogogola3@gmail.com>
2025-09-25 20:18:36 -0300

    Implementação inicial seção depoimentos

--- src/components/Depoimentos.tsx
+++ src/components/Depoimentos.tsx
@@ -1,41 +1,63 @@
 export default function Depoimentos() {
   return (
-    <section>
-      <h2 className="font-bold uppercase text-2xl text-[#4A755A]">
+    <section className="w-full mt-8">
+      <h2 className="font-bold uppercase text-2xl text-[#4A755A] text-center ">
         Depoimentos
       </h2>
-      <h3>Descubra como ajudamos pessoas a melhorar sua vida</h3>
-      <div className="flex flex-row justify-">
-        <div className="bg-[#f3f1f1] shadow-lg w-[20%] p-4">
-          <div className="font-bold">Frase</div>
-          <div className="italic mt-8">"Descricao"</div>
-          <div className="flex flex-row w-full justify-around mt-8">
+      <h3 className="text-center mt-4">Descubra como ajudamos pessoas a melhorar sua vida</h3>
+      <div className="flex flex-row justify-center gap-8 mt-4">
+        <div className="bg-[#f3f1f1] shadow-lg w-[300px] h-full p-4 flex flex-col  min-h-[350px]">
+          <div className="font-bold italic">Frase</div>
+          <div className="italic mt-8 text-[0.8rem] break-words">"DescricaoDescricaoDescricaoDescricaoDescricaoDescricaoDescricaoDescricaoDescricaoDescricaoDescricaoDescricaoDescricaoDescricaoDescricaoDescricaoDescricaoDescricaoDescricaoDescricaoDescricaoDescricaoDescricaoDescricaoDescricaoDescricaoDescricaoDescricaoDescricaoDescricaoDescricaoDescricaoDescricaoDescricaoDescricaoDescricaoDescricaoDescricaoDescricaoDescricaoDescricaoDescricaoDescricaoDescricaoDescricaoDescricaoDescricaoDescricaoDescricao"</div>
+          <div className="flex flex-row w-full p-1 justify-around mt-8 bottom-0">
             <div>
-              <img src="./perfil.png" alt="foto" className="w-[32px] rounded-full"/>
+              <img
+                src="./perfil.png"
+                alt="foto"
+                className="w-[32px] rounded-full"
+              />
             </div>
 
-            <div>nome</div>
+            <div>
+              <div className="font-semibold">Nome</div>
+              <div className="text-[0.8rem]">Idade</div>
+            </div>
           </div>
         </div>
-        <div className="bg-[#f3f1f1] shadow-lg w-[20%] p-4">
-          <div className="font-bold">Frase</div>
-          <div className="italic mt-8">"Descricao"</div>
-          <div className="flex flex-row w-full justify-around mt-8">
+        <div className="bg-[#f3f1f1] shadow-lg w-[300px] h-full p-4 flex flex-col  min-h-[350px]">
+          <div className="font-bold italic">Frase</div>
+          <div className="italic mt-8 text-[0.8rem] break-words">"DescricaoDescricaoDescricaoDescricaoDescricaoDescricaoDescricaoDescricaoDescricaoDescricaoDescricaoDescricaoDescricaoDescricaoDescricaoDescricaoDescricaoDescricaoDescricaoDescricaoDescricaoDescricaoDescricaoDescricaoDescricaoDescricaoDescricaoDescricaoDescricaoDescricaoDescricaoDescricaoDescricaoDescricaoDescricaoDescricaoDescricaoDescricaoDescricaoDescricaoDescricaoDescricaoDescricaoDescricaoDescricaoDescricaoDescricaoDescricaoDescricao"</div>
+          <div className="flex flex-row w-full p-1 justify-around mt-8 bottom-0">
             <div>
-              <img src="./perfil.png" alt="foto" className="w-[32px] rounded-full"/>
+              <img
+                src="./perfil.png"
+                alt="foto"
+                className="w-[32px] rounded-full"
+              />
             </div>
 
-            <div>nome</div>
+            <div>
+              <div className="font-semibold">Nome</div>
+              <div className="text-[0.8rem]">Idade</div>
+            </div>
           </div>
         </div>
-        <div className="bg-[#f3f1f1] shadow-lg w-[20%] p-4">
-          <div className="font-bold">Frase</div>
-          <div className="italic mt-8">"Descricao"</div>
-          <div className="flex flex-row w-full justify-around mt-8">
+        <div className="bg-[#f3f1f1] shadow-lg w-[300px] h-full p-4 flex flex-col  min-h-[350px]">
+          <div className="font-bold italic">Frase</div>
+          <div className="italic mt-8 text-[0.8rem] break-words">"DescricaoDescricaoDescricaoDescricaoDescricaoDescricaoDescricaoDescricaoDescricaoDescricaoDescricaoDescricaoDescricaoDescricaoDescricaoDescricaoDescricaoDescricaoDescricaoDescricaoDescricaoDescricaoDescricaoDescricaoDescricaoDescricaoDescricaoDescricaoDescricaoDescricaoDescricaoDescricaoDescricaoDescricaoDescricaoDescricaoDescricaoDescricaoDescricaoDescricaoDescricaoDescricaoDescricaoDescricaoDescricaoDescricaoDescricaoDescricaoDescricao"</div>
+          <div className="flex flex-row w-full p-1 justify-around mt-8 bottom-0">
             <div>
-              <img src="./perfil.png" alt="foto" className="w-[32px] rounded-full"/>
+              <img
+                src="./perfil.png"
+                alt="foto"
+                className="w-[32px] rounded-full"
+              />
             </div>
-            <div>nome</div>
+
+            <div>
+              <div className="font-semibold">Nome</div>
+              <div className="text-[0.8rem]">Idade</div>
+            </div>
           </div>
         </div>
       </div>
